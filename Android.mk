Citrus-CAF-Mod/vendor_qcom-opensource_power

power: add barrier for power hal
Change-Id: I487a00bb72b288812a188da9c30967eed06d98ee
Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_POWERHAL_VARIANT),qcom)
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
@@ -86,3 +87,4 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 endif
+endif #TARGET_POWERHAL_VARIANT

Reloaded-CAF/android_vendor_qcom_opensource_power

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_POWERHAL_VARIANT),qti)
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
@@ -91,3 +92,4 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_EXECUTABLE)
 
 endif
+endif #TARGET_POWERHAL_VARIANT